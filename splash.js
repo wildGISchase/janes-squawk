ce9e4c1bbd1942f7d19d22bbbc958a05f4e41da4
rnzee <97718336+rnzee@users.noreply.github.com>
2023-04-14 11:34:39 -0400

    Updated splash

--- splash.js
+++
@@ -1,6 +0,0 @@
-var splashScreen = document.querySelector('.splash');
-splashScreen.addEventListener('click',someFunction())
-  splashScreen.style.opacity = 0;
-  setTimeout(()=>{
-    splashScreen.classList.add('hidden')
-  },610)
