51c966fb9549e25b95365a76d52eade8dbf9aa48
rnzee <97718336+rnzee@users.noreply.github.com>
2023-05-03 17:16:03 -0400

    Ch-ch-ch-ch-changes

--- config.js
+++ config.js
@@ -1,14 +1,9 @@
 let titleDiv = "<h1>Jane's Squawk!</h1>";
 let subtitleDiv = "<h2>A (Virtual) Bird Walk Through Queens</h2>"
-let bylineDiv = "<p>with Stephen Albonesi & Ranna Zaman</p>";
+let bylineDiv = "<p>with Stephen Albonesi & Rawnak Zaman</p>";
 
 let footerDiv = 
   '<p>Created for <a href="https://www.mas.org/janes-walk-nyc-2023/" target="_blank">Jane\'s Walk 2023</a> + Find us on <a href = "https://github.com/janes-squawk/janes-squawk.github.io" target="_blank">GitHub</a>';
-
-let divChapter0 =
-  '<h2>Hello, thank you for your interest in our Jane\'s Walk!</h2>' +
-  '<p>We\'re building out the site at the moment.</p>' +
-  '<p>Please feel free to bookmark the page for now. <b>We look forward to sharing our walk with you at the start of Jane\'s Walk weekend on May 5th!</b></p>';
 
 let divChapter1 =
   '<h2>Welcome to our Jane\'s Walk! So glad you\'re joining us!</h2>' + 
@@ -21,60 +16,62 @@ let divChapter2 =
   '<p>Jane\'s Walk is one of the best weekends of the year to spot many of these birds. That\'s because the festival happens in the middle of the spring bird migration, which occurs from about March to June in New York City for most birds, with a peak in May for shorebirds and most songbirds.</p>';
 
 let divChapter3 =
-  '<div class="beside"><div class="beside2"><img src="assets/images/Atlantic-Flyway-Map-fwsgov.jpg" alt="Atlantic Flyway"><p class="imageCredit">Source: U.S. Fish & Wildlife Service</p></div>' +
-  '<p>Queens\' distinction as the <a href="https://www.nycaudubon.org/events-birding/birding-resources/birding-in-nyc/birding-in-queens" target="_blank">"birdiest borough"</a> is due in part to its location along the Atlantic Flyway, one of four major north-south routes that birds travel in North America during the spring and fall migrations. The 3,000-mile route generally starts in Greenland and the Canadian Arctic, then follows the Atlantic coast through Canada and the United States to the tropical areas of the Caribbean and South America.</p></div>';
+  '<img src="assets/images/Atlantic-Flyway-Map-fwsgov.jpg" alt="Atlantic Flyway" width=70%><p class="imageCredit">Source: U.S. Fish & Wildlife Service</p>' +
+  '<p>Queens\' distinction as the <a href="https://www.nycaudubon.org/events-birding/birding-resources/birding-in-nyc/birding-in-queens" target="_blank">"birdiest borough"</a> is due in part to its location along the Atlantic Flyway, one of four major north-south routes that birds travel in North America during the spring and fall migrations. The 3,000-mile route generally starts in Greenland and the Canadian Arctic, then follows the Atlantic coast through Canada and the United States to the tropical areas of the Caribbean and South America.</p>';
 
 let divChapter4 =
   '<h2>Queens is for Birders</h2>' +
-  '<img src="assets/images/janes-squawk-birding-jamaica-bay-wildlife-refuge.jpg" alt="Birding at Jamaica Bay Wildlife Refuge"></img>' + 
+  '<img src="assets/images/janes-squawk-birding-jamaica-bay-wildlife-refuge.jpg" alt="Birding at Jamaica Bay Wildlife Refuge" width=100%></img>' + 
   '<p>Broadly, Queens boasts <a href="https://www.nycgovparks.org/sub_about/parks_divisions/nrg/documents/NRG_Publication_A_Guide_to_the_Birds_of_Queens.pdf" target="_blank">five types of bird habitats</a>: field, forest, freshwater, salt marsh, and seashore. From the eager chirping that ushers in the early morning to flocks that manuever in neat formation over buildings and bodies of water, birds are an ever-present neighbor.</p>';
 
 let divChapter5 =
   '<h2>Habitat: Fields</h2>' +
-  '<img src="assets/images/qns-wildflower-meadow-edward-mertz-qcc.jpg">' +
+  '<img src="assets/images/qns-wildflower-meadow-edward-mertz-qcc.jpg" width=100%>' +
   '<p class="imageCredit">Photo by <a href="https://www.qcc.cuny.edu/biologicalsciences/natureoncampus/index.html" target="_blank">Edward Mertz</a></p>' +
   '<p>The growth of grasses and plants on fertile land can lead to the formation of <a href="https://www.nycaudubon.org/our-work/conservation/habitat-protection/grasslands-and-capped-landfills" target="_blank">grasslands</a>. Although they are among the <a href="https://www.nycgovparks.org/learn/ecosystems/grasslands-in-new-york-city-parks" target="_blank">rarest</a> of habitats in New York City because of human development and invasive species, fields have been allowed to thrive on many underutilized sites to the great benefit of nesting and migrating birds. Fields require maintenance for the managed growth and cultivation of native species as well as limited pesticide use. And where fields meet other habitats, like forests, the fusion of plant species attracts more animals, including birds.</p>';
 
 let divChapter6 =
   '<h2>Habitat: Forests</h2>' + 
-  '<img src="assets/images/trails-of-forest-park-raysawak-wikicommons.JPG">' + 
+  '<img src="assets/images/trails-of-forest-park-raysawak-wikicommons.JPG" width=100%>' + 
   '<p class="imageCredit">Photo by <a href="https://commons.wikimedia.org/wiki/File:Trails_of_Forest_Park.JPG" target="_blank">RaySawak</a> via Wikimedia Commons</p>' + 
   '<p>Queens\' forest land consists of many species of trees of varying maturity. This enables a diversity of bird species to take shelter and sustenance in the tall tree canopies and undergrowth beneath. <a href="https://nycaudubon.org/events-birding/birding-resources/birding-in-nyc/birding-in-queens/forest-park?&_ga=2.64092527.1737851004.1681271488-1871949272.1681271488#water-hole" target="_blank">Forest Park</a>, pictured here, is located on the site of an ancient glacier. The depressions the glacier left on the land resulted in present-day ponds, gullies, and one particularly popular “Water Hole” that draws migrating songbirds. To protect these critical breeding grounds and stopover locations for migrating birds, <a href="https://www.nycaudubon.org/our-work/conservation/habitat-protection/forests-and-upland-parks">forest conservation</a> is a necessity.</p>';
 
 let divChapter7 =
   '<h2>Habitat: Freshwater</h2>' + 
-  '<img src="assets/images/flushing-meadows-willow-lake-2019-tdorante10-wikicommons.jpg">' +
+  '<img src="assets/images/flushing-meadows-willow-lake-2019-tdorante10-wikicommons.jpg" width=100%>' +
   '<p class="imageCredit">Photo by <a href="https://commons.wikimedia.org/wiki/File:Flushing_Meadows_Willow_Lake_td_(2019-04-10)_08.jpg" target="_blank">Tdorante10</a> via Wikimedia Commons</p>' +
   '<p>Freshwater habitats include lakes, ponds, and cattail marshes. Flushing Meadows-Corona Park\'s <a href="https://www.nycgovparks.org/parks/flushing-meadows-corona-park/highlights/11312" target="_blank">Willow Lake</a>, pictured here, and the surrounding marshes and meadows attract waterfowl, hawks, and other animals. Like other habitats, freshwater wetlands are home to native plant species that provide a key migratory stop along the Atlantic flyway. Today, just <a href="https://www.nycgovparks.org/learn/ecosystems/wetlands-in-new-york-city-parks" target="_blank">one percent</a> of the city\'s historical wetlands remain, a sobering statistic given their important roles in ecosystems and stormwater flood mitigation.</p>';
 
 let divChapter8 =
   '<h2>Habitat: Salt Marshes</h2>' + 
-  '<img src="assets/images/udalls-cove-virginia-point-udallscoveorg.jpg">' +
+  '<img src="assets/images/udalls-cove-virginia-point-udallscoveorg.jpg" width=100%>' +
   '<p class="imageCredit">Photo by <a href="https://www.udallscove.org/history" target="_blank">Udalls Cove Preservation Committee</a></p>' +
   '<p>Vegetation in salt marshes has acclimated to both freshwater and saltwater, and some segments will also regularly submerge under water. One particular species of grass, <a href="https://www.nycgovparks.org/parks/udalls-park-preserve/highlights/11328" target="_blank">saltmarsh cordgrass</a>, thrives in sediment-rich deposits along the shore and provides the added benefit of acting as a natural filtration system for debris, producing mud that adds biodiversity. Like fields, many of which were once salt marshes themselves, they are also at the edges of other habitats. Wetlands such as these have to contend not only with pollution and stormwater runoff, but also <a href="https://www.nycaudubon.org/our-work/conservation/habitat-protection/coastal-wetlands-and-beaches" target="_blank">impermeable development</a> that continues to be built atop vulnerable watersheds.</p>';
 
 let divChapter9 =
   '<h2>Habitat: Seashore</h2>' +
-  '<img src="assets/images/piping-plover-and-new-chicks-wildlifenycgov.jpg">' +
+  '<img src="assets/images/piping-plover-and-new-chicks-wildlifenycgov.jpg" width=100%>' +
   '<p class="imageCredit">Photo by <a href="https://www.nyc.gov/site/wildlifenyc/animals/piping-plovers.page" target="_blank">WildlifeNYC</a></p>' +
   '<p>The Rockaway Penninsula is a hotspot for migratory birds in spring and autumn. Unfortunately, certain species of birds that nest along its beaches are threatened by encroaching human activity. On the southern side of the Breezy Point neighborhood is a federally protected area for Piping Plovers, a threatened or endangered species that has all but disappeared from some of its other habitats in the U.S.</p>';
 
 let divChapter10 = 
   '<h2>Now it\'s our turn!</h2>' +
-  '<img src="assets/images/janes-squawk-west-pond-jamaica-bay-wildlife-refuge.jpg">' +
+  '<img src="assets/images/janes-squawk-west-pond-jamaica-bay-wildlife-refuge.jpg" width=100%>' +
   '<p>With all this in mind, we set out for the West Pond Trail of Jamaica Bay Wildlife Refuge last week to see if we could observe any of the birds from our research. The approximately two-mile trail is located in Broad Channel, an inhabited island in the middle of Jamaica Bay.</p>' +
   '<p>Jamaica Bay Wildlife Refuge is a spectacular oasis comprised of 9,000 acres (20 square miles) of open bay, saltmarsh, mudflats, upland fields and woodland, small freshwater ponds, and two brackish man-made impoundments. Part of the Gateway National Recreation Area, the Refuge is the U.S. Department of the Interior\'s only wildlife refuge administered by the National Park Service.</p>';
 
 let divChapter11 = 
-  '<p>Over <a href="https://www.nycaudubon.org/events-birding/birding-resources/birding-in-nyc/birding-in-queens/jamaica-bay-wildlife-refuge" target="_blank">332 bird species</a> have been sighted at the refuge over the last 25 years, in addition to other native wildlife like horseshoe crabs, diamondback terrapin, and muskrats. Of these, approximately 70 bird species nest regularly at the refuge. In spring, Jamaica Bay is visited by numerous shorebirds, waterfowl, wading birds, terns, flycatchers, cuckoos, warblers, and tanagers, as well as the American Woodcock, which performs courtship displays by the Visitors Center at dusk.</p>' + 
+  '<p>Over <a href="https://www.nycaudubon.org/events-birding/birding-resources/birding-in-nyc/birding-in-queens/jamaica-bay-wildlife-refuge" target="_blank">332 bird species</a> have been sighted at the refuge over the last 25 years, in addition to other native wildlife like horseshoe crabs, diamondback terrapin, and muskrats. Of these, approximately 70 bird species nest regularly at the refuge. In spring, Jamaica Bay is visited by numerous shorebirds, waterfowl, wading birds, terns, flycatchers, cuckoos, warblers, and tanagers.</p>' + 
   '<p>Created in 1953 by the Parks Department to improve habitat diversity in Jamaica Bay, the brackish, 45-acre West Pond is known to be especially good for viewing migratory shorebirds in breeding plumage, including the black bellied and semipalmated plovers, red knot, and short-billed dowitcher.</p>';
 
 let divChapter12 = 
-  '<img src="assets/images/stop-1.jpeg">' +
-  '<p>We began our walk at 10 a.m. on a breezy but sunny morning in early May that felt particularly welcoming after the weekend of torrential rains that closed out the month of April. The West Pond\'s gravel trail begins in an area with dense brush and the occasional tree. It was here that we immediately heard the rapid twittering of a House Wren.</p>';
+  '<img src="assets/images/stop-1.jpeg" width=100%>' +
+  '<p>We began our walk at 10 a.m. on a breezy but sunny morning in early May that felt particularly welcoming after the weekend of torrential rains that closed out the month of April. The West Pond\'s gravel trail begins in an area with dense brush and the occasional tree. It was here that we immediately heard the rapid twittering of a House Wren.</p>' + 
+  '<img src="assets/images/test.jpg">' + 
+  '<audio controls src="assets/sounds/house-wren.wav"></audio>';
 
 let divChapter13 = 
-  '<img src="assets/images/stop-2.jpeg">' +
+  '<img src="assets/images/stop-2.jpeg" width=100%>' +
   '<p>In just a few hundred feet, we came across a clearing to the left of the trail that had a view of an expansive salt marsh and the Rockaway Peninsula in the distance. Standing stoically in the salt flats was a Great Egret, its snow white feathers contrasting with the dull brown and greens of the marsh.</p>' +
   '<p>As we gazed southward, an Osprey soared overhead. And quietly, slowly, a flock of [pipers] waded through the mud, their bills prodding the ground for insects and other food.</p>';
 
@@ -82,7 +79,7 @@ let divChapter14 =
   '<p>Continuing through the scrub brush, we were startled when a Red-winged Blackbird flew down and perched prominently on a bush branch beside the trail. Almost in curiosity, it cocked its head back and forth in between shrill "twee" calls. But our attention was quickly diverted by the stunning size of a [Ring-billed Gull or Herring Gull] that flapped overhead as it headed for the open bay.</p>';
 
 let divChapter15 = 
-  '<img src="assets/images/stop-4.jpeg">' + 
+  '<img src="assets/images/stop-4.jpeg" width=100%>' + 
   '<p>Again, the trail gave way to an expansive view, this time of West Pond itself. Acrobatically zipping and changing direction as they flew above the pond were dozens of Tree Swallows, their blue-green feathers shimmering in the sunlight as they chased flies. (At the time, we hadn\'t yet identified them by name, but  definitely marveled at the vibrancy of their feathers!).</p>' + 
   '<p>This was also our first glimpse of swimming birds; namely, Mallards and a flock of Bufflehead Ducks, with their unmistakable tufted white heads.</p>';
 
@@ -90,14 +87,14 @@ let divChapter16 =
   '<p>As we curved clockwise around the eastern edge of the pond, we caught up to several other birders in an open area surrounded by dense green scrub brush. We shared a collective moment of genuine excitement when a bright bird appeared through the foliage: a Scarlet Tanager! Its striking orange-red plumage seemed to glow as it hopped its way around the bushes. Then another appeared. We learned from our fellow birders that this was a relatively special encounter for this time of the season.</p>';
 
 let divChapter17 = 
-  '<img src="assets/images/stop-6.jpeg">' +
+  '<img src="assets/images/stop-6.jpeg" width=100%>' +
   '<p>At this point, we agreed that anything else would be icing on the cake. So as we rounded the easternmost curve of the trail, we were excited to see dozens of Brants, their white bodies and black heads bobbing among the whitecaps of the bay with the Manhattan skyline as a backdrop.</p>';
 
 let divChapter18 = 
   '<p>As we re-entered the cover of the scrub brush, the high-pitched musical sounds of a Brown Thrasher and Song Sparrow filled the still air. And while we couldn\'t see the bright plumage of the Northern Cardinal, we heard its familiar call as well.</p>';
 
 let divChapter19 = 
-  '<img src="assets/images/stop-8.jpeg">' + 
+  '<img src="assets/images/stop-8.jpeg" width=100%>' + 
   '<p>As we approached the halfway point of the walk, we encountered two familiar faces: Canada Geese. These two seemed unfazed by our presence, perhaps because their attention appeared to be on some goslings to-be.</p>';
 
 let divChapter20 = 
@@ -133,25 +130,7 @@ var config = {
   footer: footerDiv,
   chapters: [
     {
-      id: "under-constr",
-      alignment: "centered",
-      hidden: false,
-      chapterDiv: divChapter0,
-      location: {
-        center: [-74, 40.725],
-        zoom: 10,
-        zoomSmall: 9,
-        pitch: 0,
-        bearing: 0,
-      },
-      mapAnimation: "flyTo",
-      rotateAnimation: false,
-      callback: "",
-      onChapterEnter: [],
-      onChapterExit: [],
-    },
-    {
-      id: "1-welcome",
+      id: "welcome",
       alignment: "centered",
       hidden: false,
       chapterDiv: divChapter1,
@@ -162,20 +141,15 @@ var config = {
         pitch: 0,
         bearing: 0,
       },
-      mapAnimation: "flyTo",
-      rotateAnimation: false,
-      callback: "",
-      onChapterEnter: [],
-      onChapterExit: [],
     },
     {
-      id: "2-context-i",
-      alignment: "left",
+      id: "context-1",
+      alignment: "right",
       hidden: false,
       chapterDiv: divChapter2,
       location: {
-        center: [-74, 40.725],
-        zoom: 10,
+        center: [-73.75160, 40.67468],
+        zoom: 10.8,
         zoomSmall: 9,
         pitch: 0,
         bearing: 0,
@@ -199,8 +173,8 @@ var config = {
       ],
     },
     {
-      id: "2-context-ii",
-      alignment: "full",
+      id: "context-2",
+      alignment: "centered",
       hidden: false,
       chapterDiv: divChapter3,
       location: {
